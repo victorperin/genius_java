9fafdfb34635f11405ca5d0ec7db405a38f995b1
Victor Perin <me@victorperin.com>
2015-09-07 01:45:30 -0300

    inserindo tela do menu

---
+++ Menu.java
@@ -0,0 +1,10 @@
+public class Menu extends JFrame implements ActionListener{
+  private static final long serialVersionUID = 1L;
+
+  public Menu(){
+    super("Menu Principal");
+  }
+  public void actionPerformed(ActionEvent evento){
+  }
+
+}
